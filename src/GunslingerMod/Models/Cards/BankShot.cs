5d75d94b8b18b293ed69f85d52e08e20f75f85e8
HamelX <161572407+HamelX@users.noreply.github.com>
2026-03-28 01:34:04 +0900

    Rebalance Gunslinger around Ricochet and Tracer core axes

---
+++ src/GunslingerMod/Models/Cards/BankShot.cs
@@ -0,0 +1,46 @@
+using System;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using GunslingerMod.Models.Combat;
+using GunslingerMod.Models.Powers;
+
+namespace GunslingerMod.Models.Cards;
+
+public sealed class BankShot() : CardModel(1, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+{
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target);
+
+        var cylinder = Owner.Creature.GetPower<CylinderPower>();
+        if (cylinder == null)
+            return;
+
+        var hasRicochet =
+            (Owner.Creature.GetPower<RicochetPower>()?.Amount ?? 0) > 0 ||
+            (Owner.Creature.GetPower<RicochetImprintPower>()?.Amount ?? 0) > 0;
+
+        var triggerPulls = 1 + (hasRicochet ? 1 : 0) + (IsUpgraded ? 1 : 0);
+
+        for (var i = 0; i < triggerPulls; i++)
+        {
+            if (!BulletResolver.HasAliveOpponents(Owner.Creature))
+                break;
+
+            var target = BulletResolver.ResolveAliveTarget(Owner.Creature, cardPlay.Target);
+            if (target == null)
+                break;
+
+            var didFire = BulletResolver.TryConsumeCurrentWithSealSkip(cylinder, this, out var ammoType, out var sealLevel);
+            await PowerCmd.SetAmount<CylinderPower>(Owner.Creature, cylinder.CountLoaded(), Owner.Creature, this);
+
+            if (!didFire)
+                continue;
+
+            var damage = Math.Max(0m, BulletResolver.GetBaseDamage(ammoType, sealLevel));
+            await BulletResolver.FireAtTarget(choiceContext, Owner.Creature, target, this, ammoType, sealLevel, damage);
+        }
+    }
+}
---
+++ src/GunslingerMod/Models/Cards/TracerStrike.cs
@@ -0,0 +1,43 @@
+using System;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using GunslingerMod.Models.Combat;
+using GunslingerMod.Models.Powers;
+
+namespace GunslingerMod.Models.Cards;
+
+public sealed class TracerStrike() : CardModel(1, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
+{
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target);
+
+        var cylinder = Owner.Creature.GetPower<CylinderPower>();
+        if (cylinder == null)
+            return;
+
+        var tracerLoads = IsUpgraded ? 2 : 1;
+        for (var i = 0; i < tracerLoads; i++)
+            cylinder.TryLoadNext(CylinderPower.AmmoType.Tracer);
+
+        await PowerCmd.SetAmount<CylinderPower>(Owner.Creature, cylinder.CountLoaded(), Owner.Creature, this);
+
+        if (!BulletResolver.HasAliveOpponents(Owner.Creature))
+            return;
+
+        var target = BulletResolver.ResolveAliveTarget(Owner.Creature, cardPlay.Target);
+        if (target == null)
+            return;
+
+        var didFire = BulletResolver.TryConsumeCurrentWithSealSkip(cylinder, this, out var ammoType, out var sealLevel);
+        await PowerCmd.SetAmount<CylinderPower>(Owner.Creature, cylinder.CountLoaded(), Owner.Creature, this);
+
+        if (!didFire)
+            return;
+
+        var damage = Math.Max(0m, BulletResolver.GetBaseDamage(ammoType, sealLevel));
+        await BulletResolver.FireAtTarget(choiceContext, Owner.Creature, target, this, ammoType, sealLevel, damage);
+    }
+}
